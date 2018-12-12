6b8f9ad2f7010ee1e0c74d94170ca30b78c1fd54
ColeH <cbhreads2@gmail.com>
2018-12-12 11:09:25 -0500

    Added the ability to set a hex code nametag (toggleable in config)

--- bot.js
+++ bot.js
@@ -1165,14 +1165,90 @@ var e = [];
 				  
 			  });
 			}
-        } else if (colorRoleList(evt).length > 0) {
+        } else if(args[0].toUpperCase().replace('#','').match(/^([0-F]){6}$/)) {
+            if(_cfg.enabledFeatures.namecolor_hex) {
+                var hexCode = args[0].replace('#','').match(/^([0-F]){6}$/).input;
+                if(!hexCode) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "Okay, if you're seeing this error, something has gone VERY WRONG. DM coleh#1346 ASAP",
+                    });
+                } else {
+                   var hexDecNum = parseInt(hexCode,16);
+                   function hexColorDelta(hex1, hex2) {
+                        // get red/green/blue int values of hex1
+                        var r1 = parseInt(hex1.substring(0, 2), 16);
+                        var g1 = parseInt(hex1.substring(2, 4), 16);
+                        var b1 = parseInt(hex1.substring(4, 6), 16);
+                        // get red/green/blue int values of hex2
+                        var r2 = parseInt(hex2.substring(0, 2), 16);
+                        var g2 = parseInt(hex2.substring(2, 4), 16);
+                        var b2 = parseInt(hex2.substring(4, 6), 16);
+                        // calculate differences between reds, greens and blues
+                        var r = 255 - Math.abs(r1 - r2);
+                        var g = 255 - Math.abs(g1 - g2);
+                        var b = 255 - Math.abs(b1 - b2);
+                        // limit differences between 0 and 1
+                        r /= 255;
+                        g /= 255;
+                        b /= 255;
+                        // 0 means opposit colors, 1 means same colors
+                        return (r + g + b) / 3;
+                    }
+                    if(hexColorDelta(hexCode, '140A02') < 0.8) {
+                        if(colorRoleObjList(evt).find(x => { return hexColorDelta(x.color.toString(16),hexCode) > 0.95 })) {
+                            bot.addToRole({
+                            serverID: evt.d.guild_id,
+                            userID: evt.d.author.id,
+                            roleID: colorRoleObjList(evt).find(x => { return hexColorDelta(x.color.toString(16),hexCode) > 0.95 }).id 
+                            }); 
+                              bot.sendMessage({
+                                to: channelID,
+                                message: 'Your nametag color has been set! *Please note that your color has been \'snapped\' to a preexisting color I found, which was more than 95% the same as yours.'
+                                  
+                              });
+                        } else {
+                            bot.createRole(evt.d.guild_id, function(err,resp) { 
+                                if(err) { console.log(err); return }
+                                //console.log(resp);
+                                var JustMadeRole = resp.id;
+                                bot.editRole({serverID:evt.d.guild_id,roleID:JustMadeRole,name: '#' + hexCode + ' Nametag',color:hexDecNum});
+                                if(JustMadeRole) { 
+                                    bot.addToRole({
+                                    serverID: evt.d.guild_id,
+                                    userID: evt.d.author.id,
+                                    roleID: JustMadeRole 
+                                    }); 
+                                      bot.sendMessage({
+                                        to: channelID,
+                                        message: 'Your nametag color has been set!'
+                                          
+                                      });
+                                }
+                            });
+                        }
+                    } else {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "Oh no, it looks like that color is too close to Discord Grey. Please pick a more contrasting color :smiley:",
+                        });
+                    }
+                    
+                }
+            } else {
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Oops, custom hex codes for nametags aren't supported on this server.",
+                });
+            }
+        }else if (colorRoleList(evt).length > 0) {
 		var clrRoleList = colorRoleList(evt);
 		if(args.length > 0 && ( args[0] != 'none' && args[0] != 'clear') ) {
 		bot.sendMessage({
 			to: channelID,
 			message: "That's not a valid color here! I'll DM you a list of colors that work on this server.",
 		});
-		//Generate image of colors (WIP) 
+		//Generate image of colors 
 
 var canvas = createCanvas(100,(clrRoleList.length*40));
 var ctx = canvas.getContext('2d');
