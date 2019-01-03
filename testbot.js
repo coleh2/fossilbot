76af8f4c20f8dfcdbf1df712971cd3b798d28035
coleh <cbhreads2@gmail.com>
2019-01-02 20:53:40 -0500

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -6,6 +6,6 @@ bot.connect();
 bot.on('ready', function() {
 bot.sendMessage({
 	to: '521345011758006274',
-	message: 'This is a confirmation that Cole hacked Adam\'s account'
+	message: ''
 });
 })
