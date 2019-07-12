b37a02477dac296a1da3daffb86510c92d8b55df
coleh <coleh@coleh.net>
2019-07-12 14:20:22 -0400

    i am typing this with my hands

--- modules/commandmanager/commandmanager.js
+++ modules/commandmanager/commandmanager.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
-var filenamearray = fs.readdirSync(__dirname + '/modules/commands'),
+var filenamearray = fs.readdirSync(__dirname + '/../commands'),
     requiredfileobject = {};
-var aliases = require(__dirname + '/modules/commandmanager/command_aliases.json');
+var aliases = require(__dirname + '/command_aliases.json');
 
 for(var i = 0; i < filenamearray.length; i++) {
     var fileNameWithoutExtension = filenamearray[i].split('\.')[0];
-    requiredfileobject[fileNameWithoutExtension] = require(__dirname + '/modules/commands/' + filenamearray[i]);
+    requiredfileobject[fileNameWithoutExtension] = require(__dirname + '/../commands/' + filenamearray[i]);
     if(aliases[fileNameWithoutExtension]) {
         var aliasedEntry = aliases[fileNameWithoutExtension];
         for(var aliasI = 0; aliasI < aliasedEntry.length; aliasI++) {
